2f1eebeec78b8258bb55901a060b4409bfb13802
Gabriela Torres <>
2023-11-28 01:03:07 -0500

    Añadir boton de cerrar sesion

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const usuario = JSON.parse(localStorage.getItem('usuario'));
+
+if (usuario) {
+    const noUsuarioMenu = document.querySelector('.no-usuario');
+    const conUsuarioMenu = document.querySelector('.con-usuario');
+    noUsuarioMenu.classList.add('ocultar');
+    conUsuarioMenu.classList.remove('ocultar')
+
+    document.querySelector('#usuario-nombre').innerText = usuario.nombre;
+
+    document.querySelector('#cerrar-sesion').addEventListener('click', () => {
+        localStorage.removeItem('usuario');
+        window.location = '/index.html';
+    })
+}
